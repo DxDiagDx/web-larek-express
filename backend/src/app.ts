54d35d1af35957265383848ceefca8a1453b9a6d
Evgeny <lukin@usota.ru>
2025-08-07 14:22:12 +0300

    Реализация requestLogger и errorLogger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import path from 'path';
 import errorHandler from './middlewares/error';
 import productsRouter from './routes/products';
 import ordersRouter from './routes/orders';
+import { requestLogger, errorLogger } from './middlewares/logger'
 
 const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek' } = process.env;
 
@@ -14,10 +15,15 @@ mongoose.connect(DB_ADDRESS);
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(requestLogger);
 app.use('/product', productsRouter);
 app.use('/order', ordersRouter);
+app.use(errorLogger);
+
 app.use(errors());
 app.use(errorHandler);
 
